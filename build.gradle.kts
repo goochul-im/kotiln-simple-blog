a38a157cb3c0b8e2b5219a256459eaab9de3b501
goochul-im <goochul175465@gmail.com>
2025-08-29 01:15:32 +0900

    build(aop): aop 패키지 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("com.linecorp.kotlin-jdsl:spring-data-jpa-support:3.5.5")
     implementation("com.linecorp.kotlin-jdsl:jpql-dsl:3.5.5")
     implementation("com.linecorp.kotlin-jdsl:jpql-render:3.5.5")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
 }
 
 kotlin {
